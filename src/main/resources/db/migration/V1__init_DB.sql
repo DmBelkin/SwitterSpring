46aff8d6dbc09e0b84b3e7d619db4ad07a2a9ea7
DmBelkin <143951259+DmBelkin@users.noreply.github.com>
2023-09-15 00:39:49 +0300

    with correct properties

--- src/main/resources/db/migration/V1__init_DB.sql
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -1,55 +1,21 @@
+create table message
+ (id bigint not null,
+  filename varchar(255),
+   tag varchar(255),
+    text varchar(255),
+     user_id bigint,
+      primary key (id))
+       engine=MyISAM
 
-CREATE TABLE message
-(
-  `id`       int not null,
-  `filename` varchar(255),
-  `tag`      varchar(255),
-  `text`     varchar(2048) not null ,
-  `user_id`  bigint,
-primary key(id)
-);
+create table message_seq
+(next_val bigint)
+ engine=MyISAM
 
-create table `message_seq`
-(
-  `next_val` bigint
-);
+insert into message_seq values ( 1 )
 
-insert into `message_seq`
-values (1);
-
-create table `user`
-(
-  `id`            int not null,
-  activation_code varchar(255),
-  `active`        boolean  not null,
-  `email`         varchar(255) not null ,
-  `password`      varchar(255) not null ,
-  `username`      varchar(255) not null ,
-primary key(id)
-);
-
-create table `user_role`
-(
-  `user_id` bigint not null,
-  role      varchar(255)
-);
-
-create table `user_seq`
-(
-  `next_val` bigint
-);
-
-insert into `user_seq`
-values (1);
-
-insert into `user`
-values (1, 0, true, "some@some.com" ,123, "sidor" );
-
-insert into `user_role`
-values (1 ,  "ADMIN");
-
-alter table message
-add constraint FKb3y6etti1cfougkdr0qiiemgv foreign key (`user_id`) references user (id);
-
-alter table user_role
-add constraint FK859n2jvi8ivhui0rl0esws6o foreign key (`user_id`) references user (id)
+create table user (id bigint not null, activation_code varchar(255), active bit not null, email varchar(255), password varchar(255), username varchar(255), primary key (id)) engine=MyISAM
+create table user_role (user_id bigint not null, role varchar(255)) engine=MyISAM
+create table user_seq (next_val bigint) engine=MyISAM
+insert into user_seq values ( 1 )
+alter table message add constraint FKb3y6etti1cfougkdr0qiiemgv foreign key (user_id) references user (id)
+alter table user_role add constraint FK859n2jvi8ivhui0rl0esws6o foreign key (user_id) references user (id)
